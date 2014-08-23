911cb523ff068bb3df1921258c22f7c8ce054b3c
oleg.bezk <ultramarine77777@gmail.com>
2014-08-23 22:25:40 +0300

    chat serv

--- src/main/java/com/gl/jeetraining/webapp/service/ChatService.java
+++ src/main/java/com/gl/jeetraining/webapp/service/ChatService.java
@@ -1,5 +1,39 @@
 package com.gl.jeetraining.webapp.service;
 
-public class ChatService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gl.jeetraining.webapp.dao.IChatDAO;
+import com.gl.jeetraining.webapp.model.Chat;
+
+@Service
+@Scope("request")
+@Transactional(readOnly = true)
+public class ChatService implements IChatService {
+
+	@Autowired
+	private IChatDAO chatDAO;
+
+	public IChatDAO getChatDAO() {
+		return chatDAO;
+	}
+
+	public void setChatDAO(IChatDAO chatDAO) {
+		this.chatDAO = chatDAO;
+	}
+
+	@Override
+	public Chat getChat(int id) {
+		return chatDAO.getChat(id);
+	}
+
+	@Override
+	public List<Chat> getChats() {
+		return getChatDAO().getChats();
+	}
 
 }
--- src/main/java/com/gl/jeetraining/webapp/service/IChatService.java
+++ src/main/java/com/gl/jeetraining/webapp/service/IChatService.java
@@ -1,9 +1,15 @@
 package com.gl.jeetraining.webapp.service;
 
+import java.util.List;
+
 import com.gl.jeetraining.webapp.model.Chat;
 
 public interface IChatService {
 	
 	public Chat getChat(int id);
+	
+	public List<Chat> getChats();
+	
+	
 
 }
